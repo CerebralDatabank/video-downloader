7db044b99a5e7ab53f84c878d91f3f0d9d2c472f
Gopal Othayoth <24465111+CerebralDatabank@users.noreply.github.com>
2020-06-09 14:45:44 -0500

    Add test script for /downloader path

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (request, response) => {
   response.sendFile("./index.html", {root: __dirname});
 });
 
+app.get("/downloader", (request, response) => {
+  response.send(JSON.stringify(request));
+});
+
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is listening at process.env.PORT (${process.env.PORT || "unknown"}) or port 4000`);
 });
