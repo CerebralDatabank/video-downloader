03ec1ef70e0c02f558b0d61596cdf8551fea9230
Gopal Othayoth <24465111+CerebralDatabank@users.noreply.github.com>
2020-06-09 13:36:08 -0500

    Re-added port listener

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ const app = express();
 app.get("/", (request, response) => {
   response.sendFile("index.html");
 });
+
+app.listen(process.env.port || 4000, () => {
+  console.log("Server is listening at default port or port 4000");
+});
